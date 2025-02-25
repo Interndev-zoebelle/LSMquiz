b3e157896ae18ae3c121ef993a5552145d2fdaac
Interndev-zoebelle <interndev@ohceans.com>
2025-02-25 10:09:49 +0800

    add user icon and sign in/ sign up button at navigation bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,15 @@
-import { home } from "@/utils/Icons";
-import { chart } from "../utils/Icons";
+"use client";
+import { chart, home, login } from "@/utils/Icons";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
-import Link from 'next/link';
-import Image from 'next/image';
+import { Button } from "./ui/button";
 
 function Header() {
+
+    const pathname = usePathname();
 
     const menu = [
         {
@@ -29,13 +34,51 @@ function Header() {
             <ul className="flex items-center gap-8">
                 {menu.map((item, index) => (
                     <li key={index}>
-                        <Link href={item.link}>
-                        <span>{item.icon}</span>
-                        <span>{item.name}</span>
-                        </Link>
+                    <Link
+                        href={item.link}
+                        className={`py-1 px-6 flex items-center gap-2 text-lg leading-none text-gray-400 rounded-lg
+                        ${
+                            pathname === item.link
+                            ? "bg-blue-500/20 text-blue-400 border-2 border-blue-400"
+                            : ""
+                        }
+                        
+                        `}
+                    >
+                        <span className="text-2xl text-blue-400">{item.icon}</span>
+                        <span
+                        className={`font-bold uppercase
+                        ${pathname === item.link ? "text-blue-400" : "text-gray-400"}
+                        `}
+                        >
+                        {item.name}
+                        </span>
+                    </Link>
                     </li>
                 ))}
-            </ul>
+                </ul>
+
+                <div>
+                    <SignedIn>
+                        <UserButton
+                        appearance={{
+                            elements: {
+                                userButtonAvatarBox: "w-12 h-12 boarder-2 rounded-full",
+                            },
+                        }}
+                        />
+                    </SignedIn>
+                    <SignedOut>
+                        <Button
+                        className="py-5 bg-blue-400 flex items-center gap-2 font-semibold text-lg rounded-lg
+                        hover:bg-blue-500/90"
+                        onClick={() => router.push("/sign-in")}
+                        >
+                        {login}
+                        Login / Sign Up
+                        </Button>
+                    </SignedOut>
+                </div>
          </nav>
       </header>
     );
